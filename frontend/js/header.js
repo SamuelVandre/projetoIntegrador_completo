1b54f3300a60b1a58a1a9f267e48d1f8dc0c48b1
Gustavo Poeta <gustavpoeta@gmail.com>
2026-06-09 16:21:28 -0300

    feat: faz mudanças em serviços, home e projetos. Preparando o site para a apresentação

---
+++ frontend/js/header.js
@@ -0,0 +1,8 @@
+<script>
+const hamburguer = document.getElementById("hamburguer");
+const menu = document.getElementById("menu");
+
+hamburguer.addEventListener("click", () => {
+    menu.classList.toggle("ativo");
+});
+</script>
--- frontend/pages/servicos.php
+++ frontend/pages/servicos.php
@@ -24,7 +24,7 @@
       <p class="hero__subtitulo">NAYARA LEAL ARQUITETURA</p>
       <h1 class="hero__titulo">Serviços</h1>
       <p class="hero__descricao">Transformamos ambientes em experiências sofisticadas, funcionais e atemporais.</p>
-      <a href="orcamentos.html" class="hero__botao">SOLICITAR ORÇAMENTO</a>
+      <a href="orcamentos.php" class="hero__botao">SOLICITAR ORÇAMENTO</a>
     </div>
   </section>
 
@@ -247,36 +247,13 @@
     <p class="cta__subtitulo">VAMOS CONVERSAR</p>
     <h2 class="cta__titulo">Pronto para transformar<br>seu espaço?</h2>
     <p class="cta__descricao">Entre em contato e solicite um orçamento personalizado.</p>
-    <a href="orcamentos.html" class="cta__botao">SOLICITAR ORÇAMENTO</a>
+    <a href="orcamentos.php" class="cta__botao">SOLICITAR ORÇAMENTO</a>
   </section>
 
 
-  <!-- RODAPÉ -->
-  <footer class="rodape">
-    <div class="rodape__conteudo">
-
-      <div class="rodape__contatos">
-        <p class="rodape__titulo">Contatos:</p>
-        <p>exemplo@email.com</p>
-        <p>(19) 1001010101</p>
-        <p>(endereço)</p>
-      </div>
-
-      <div class="rodape__redes">
-        <p class="rodape__titulo">Redes Sociais:</p>
-        <div class="rodape__links">
-          <a href="https://www.linkedin.com/in/arq-nayaraleal/">LinkedIn</a>
-          <a href="https://www.instagram.com/arquitetanayaraleal/">Instagram</a>
-        </div>
-      </div>
-
-    </div>
-
-    <div class="rodape__base">
-      <p>Copyright © 2026 – Nayara Leal Arquitetura – Todos os direitos reservados</p>
-      <a href="https://www.gov.br/mdh/pt-br/acesso-a-informacao/politica-de-privacidade">Políticas de Privacidade</a>
-    </div>
-  </footer>
+  <?php
+    include_once('../components/footer.php'); 
+  ?>
 
 </body>
 </html>
